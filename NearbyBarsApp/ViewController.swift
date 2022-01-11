f052b73087c4423e76824ed4cba5122e01daf80d
Kiana Dyson <kdyson@calpoly.edu>
2022-01-10 20:42:56 -0500

    Initial Upload

---
+++ NearbyBarsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearbyBarsApp
+//
+//  Created by Kiana Dyson on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
